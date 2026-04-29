d4d0aaa4797f84239faf776c19f0858f0ba3caa4
José Allamberg <joseallamberg13@gmail.com>
2026-04-29 09:01:16 -0300

    Delete backend directory

--- backend/src/models/Categoria.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model } = require('mongoose');
-
-const categoriaSchema = new Schema({
-  nome: { type: String, required: true, trim: true },
-  horasLimite: { type: Number, required: true },
-  cursoId: { type: Schema.Types.ObjectId, ref: 'Curso', required: true },
-}, { timestamps: false });
-
-module.exports = model('Categoria', categoriaSchema);
--- backend/src/routes/auth.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const authController = require('../controllers/authController');
-
-router.post('/login', authController.login);
-router.put('/reset-senha', authController.resetSenha);
-
-module.exports = router;
