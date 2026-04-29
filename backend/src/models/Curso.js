b3bfac3d98008b79a14a1cfd3a05748fd60e85fb
José Allamberg <joseallamberg13@gmail.com>
2026-04-29 09:06:14 -0300

    Delete backend directory

--- backend/src/models/Curso.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model } = require('mongoose');
-
-const cursoSchema = new Schema({
-  nome: { type: String, required: true, trim: true },
-  horasExigidas: { type: Number, required: true },
-  horasPorSemestre: { type: Number, default: 25 },
-}, { timestamps: false });
-
-module.exports = model('Curso', cursoSchema);
--- backend/src/routes/dashboard.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const dashboardController = require('../controllers/dashboardController');
-const { autenticar } = require('../middlewares/auth');
-
-router.get('/admin', autenticar, dashboardController.dashboardAdmin);
-router.get('/coordenador/:cursoId', autenticar, dashboardController.dashboardCoordenador);
-
-module.exports = router;
